04f47c07857647b6c715f6b8a32dbdc60853c47e
Camillaaaaaaa <c.scholz@student.utwente.nl>
2024-05-01 10:28:53 +0200

    commit

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-//import { ImageSegmenter, SegmentationMask, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2";
+import * as bodySegmentation from '@tensorflow-models/body-segmentation';
+import * as tf from '@tensorflow/tfjs-core';
+// Register WebGL backend.
+import '@tensorflow/tfjs-backend-webgl';
 
-async function createImageSegmenter() {
-  const vision = await FilesetResolver.forVisionTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
-  );
 
-  imageSegmenter = await ImageSegmenter.createFromOptions(vision,{
-    baseOptions:{
-      modelAssetPath:
-      "https://storage-googleapis.com/mediapipe-models/image_segmenter/selfie_segmenter/float16/latest/selfie_segmenter.tflite",
-      delegate:"GPU"
-    },
-    outputCategoryMask:true,
-    outpuConfidenceMasks:false,
-    runningMode:"IMAGE"
-  })
-}
-
-createImageSegmenter();
+const model = bodySegmentation.SupportedModels.MediaPipeSelfieSegmentation;
+const segmenterConfig = {
+  runtime: 'tfjs',
+};
+segmenter = await bodySegmentation.createSegmenter(model, segmenterConfig);
